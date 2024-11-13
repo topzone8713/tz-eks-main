f75ac15b6ed2e68484df1ce2df69fb9f129ff66c
doohee323 <doohee323@gmail.com>
2024-11-12 18:52:23 -0800

    add awscc_ecr_repository tz-demo-app

--- terraform-aws-eks/workspace/base/app.tf
+++ terraform-aws-eks/workspace/base/app.tf
@@ -5,3 +5,11 @@ resource "awscc_ecr_repository" "tz-devops-admin" {
     scan_on_push = true
   }
 }
+
+resource "awscc_ecr_repository" "tz-demo-app" {
+  repository_name      = "tz-demo-app"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration = {
+    scan_on_push = true
+  }
+}
